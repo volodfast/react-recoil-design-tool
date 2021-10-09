9d136db0260638283948a575229d012d53a1ea44
Volodymyr Bystranivskyi <volodfast@gmail.com>
2021-10-09 23:28:29 +0300

    Restructure project components

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/core/App/App';
+import App from './modules/core/components/App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
