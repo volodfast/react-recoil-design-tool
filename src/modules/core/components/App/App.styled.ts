be0fc68cf841583e9e4fd5e3a626de0778a13a5c
Volodymyr Bystranivskyi <volodfast@gmail.com>
2021-10-09 23:32:58 +0300

    Add global styles

--- src/modules/core/components/App/App.styled.ts
+++ src/modules/core/components/App/App.styled.ts
@@ -1,8 +1,25 @@
-import styled from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components';
+import { colors } from '../../constants';
 
 export const AppContainer = styled.div`
   display: flex;
   flex-direction: row;
   width: 100vw;
   height: 100vh;
+`;
+
+export const GlobalStyles = createGlobalStyle`
+    body {
+        color: white;
+        margin: 0;
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+            'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+            sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        background-color: ${colors.darkGray};
+    }
+    * {
+        box-sizing: border-box;
+    }
 `;
