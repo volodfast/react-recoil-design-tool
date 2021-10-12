b34d9765c12d3b4c46816f07a7ca3fc954260bf0
Volodymyr Bystranivskyi <volodfast@gmail.com>
2021-10-13 01:12:22 +0300

    Add design-tool page

---
+++ src/pages/design-tool/design-tool.tsx
@@ -0,0 +1,19 @@
+import React, { FC } from 'react';
+// components
+import Canvas from '../../modules/design-tool/components/Canvas';
+import LeftSidebar from '../../modules/design-tool/components/LeftSidebar';
+import RightSidebar from '../../modules/design-tool/components/RightSidebar';
+// styles
+import { DesignToolPageContainer } from './design-tool.styled';
+
+const DesignToolPage: FC = () => {
+  return (
+    <DesignToolPageContainer>
+      <LeftSidebar />
+      <Canvas />
+      <RightSidebar />
+    </DesignToolPageContainer>
+  );
+};
+
+export default DesignToolPage;
