9f011754b22273b0a2789cf015f8f983e1a06c56
Volodymyr Bystranivskyi <volodfast@gmail.com>
2021-10-14 23:12:09 +0300

    Update Image component to show image

--- src/modules/design-tool/components/Image/Image.tsx
+++ src/modules/design-tool/components/Image/Image.tsx
@@ -1,12 +1,21 @@
 import React, { FC } from 'react';
+import { useRecoilValue } from 'recoil';
+import { imageDimensionsState, imageSrcState } from '../../design-tool.state';
 // interfaces
 import { ImageProps } from './Image.interface';
 
-const Image: FC<ImageProps> = () => {
+const Image: FC<ImageProps> = ({ id }) => {
+  const imageSrc = useRecoilValue(imageSrcState(id));
+  const dimensions = useRecoilValue(imageDimensionsState(id));
+
+  if (!imageSrc) {
+    return null;
+  }
+
   return (
     <div
       style={{
-        backgroundImage: "url('')",
+        backgroundImage: `url('${imageSrc}')`,
         backgroundSize: '100% 100%',
         flex: 1,
       }}
